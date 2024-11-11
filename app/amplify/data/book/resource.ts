451048bb4f121d1d75f85a0ebe86b68977e3da69
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2024-11-11 21:07:56 +0100

    Work

--- app/amplify/data/book/resource.ts
+++
@@ -1,7 +0,0 @@
-import { defineFunction, secret } from '@aws-amplify/backend'
-
-export const book = defineFunction({
-  environment: {
-    connectionString: secret('SQL_CONNECTION_STRING'),
-  },
-})
